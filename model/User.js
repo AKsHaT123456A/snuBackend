583be1e0f442c3558295a3e6776d9194b703b600
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-01-19 19:09:42 +0530

    API

---
+++ model/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type:String,required: true, unique: 1 },
+    password: { type:String,required: true },
+    isAdmin: { type: Boolean, default: false },
+    email:{type:String}
+  },
+  { timestamps: true }
+);
+const User1=mongoose.model("User",UserSchema);
+module.exports=User1;
