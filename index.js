a95339a22eaaba19a0e0a63710a80b8f81ed58b7
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-01-22 02:21:29 +0530

    Revised

--- index.js
+++ index.js
@@ -17,49 +17,9 @@ mongoose
   })
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
-app.use("/auth", authRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
-app.post("/register", async (req, res) => {
-  const otp = Math.floor(Math.floor(100000 + Math.random() * 900000));
-  const newUser = User1({
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SECRET_KEY
-    ).toString(),
-    otp:otp
-  });
-  try {
-    const user = await newUser.save();
-    // emailer(user.email,user.otp);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-//LOGIN
-app.post("/login", async (req, res) => {
-  try {
-    const user = await User1.findOne({ email: req.body.email });
-    !user && res.status(401).json("Email or Username not found!");
-    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-    originalPassword !== req.body.password &&
-      res.status(401).json("Wrong Password");
-    const accessToken = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.SECRET_KEY,
-      { expiresIn: "1d" }
-    );
-    const { password, ...info } = user._doc;
-    res.status(201).json({...info, accessToken});
-  } catch (error) {
-    res.status(500).json(error);
-
-  }
-});
 app.listen(port, () => {
   console.log(`Sever running at ${port}`);
 });
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,46 @@ const jwt = require("jsonwebtoken");
 const CryptoJS = require("crypto-js");
 const User1 = require("../model/User");
 //REGISTER
+router.post("/register", async (req, res) => {
+  const otp = Math.floor(Math.floor(100000 + Math.random() * 900000));
+  const newUser = User1({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+    otp:otp
+  });
+  try {
+    const user = await newUser.save();
+    // emailer(user.email,user.otp);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User1.findOne({ email: req.body.email });
+    !user && res.status(401).json("Email or Username not found!");
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong Password");
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+    const { password, ...info } = user._doc;
+    res.status(201).json({...info, accessToken});
+  } catch (error) {
+    res.status(500).json(error);
 
+  }
+});
 //verification
 router.patch("/register/verify", async (req, res) => {
   try {
