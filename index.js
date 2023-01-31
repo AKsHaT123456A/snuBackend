92d8508ddcf77f42ec66f39211b12cf26e436de9
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-01-31 23:35:35 +0530

    CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 const express = require("express");
+const cors=require("cors")
 const path =require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -9,8 +10,17 @@ const movieRoute = require("./AdminRoutes/movieRoute");
 const emailRoute = require("./AdminRoutes/emailRoutes");
 mongoose.set("strictQuery", true);
 require("dotenv").config();
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 const PORT = 3000 || process.env.PORT;
 mongoose
   .connect(process.env.DATABASE_KEY, {
